fccb2477d9c24f90e214657e04877ad6fd087b65
Amine Sehlaoui <a.sehlaoui96@gmail.com>
2024-05-22 19:05:28 +0100

    adjusted the table : added content + install @MUI react material

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -52,28 +52,29 @@ function Dashboard({Toggle}) {
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Product</th>
+            <th scope="col">Price</th>
+            <th scope="col">Expiration date</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Milk-Jaouda</td>
+            <td>04.00 MAD</td>
+            <td>29/05/2024</td>
           </tr>
           <tr>
             <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>Tea</td>
+            <td>13.50 MAD</td>
+            <td>31/05/2024</td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
+            <td>Vegetal Oil</td>
+            <td>95.00 MAD</td>
+            <td>26/08/2025</td>
           </tr>
         </tbody>
       </table>
